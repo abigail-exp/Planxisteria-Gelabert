2587b7ffcd980781a284bb145c4f39c0c33320fc
Big-Lolo <95545807+Big-Lolo@users.noreply.github.com>
2025-06-16 01:39:49 +0200

    Project
    -Faltaria secció contactes + footer

---
+++ script.js
@@ -0,0 +1,58 @@
+//Codi carrussel (Eliminat el automoviment, per tornar-ho a ficar, només ficar un setInterval)
+const images = [
+    {title: "Galeria1", src: "./assets/img/galeria1.png"},
+    {title: "Galeria2", src: "./assets/img/galeria2.png"},
+    {title: "Galeria3", src: "./assets/img/galeria3.png"},
+    {title: "Galeria4", src: "./assets/img/galeria4.png"},
+    {title: "Galeria5", src: "./assets/img/galeria5.png"},
+    {title: "Galeria6", src: "./assets/img/galeria6.png"},
+    {title: "Galeria7", src: "./assets/img/galeria7.png"},
+    {title: "Galeria8", src: "./assets/img/galeria8.png"}
+];
+
+let current = 0;
+const carousel = document.getElementById('carousel');
+function renderCarousel() {
+    carousel.innerHTML = '';
+    for (let i = 0; i < 4; i++) {
+        const idx = (current + i) % images.length;
+        const div = document.createElement('div');
+        div.className = "bg-white flex flex-col w-1/4 h-64 rounded shadow-lg overflow-hidden";
+        const img = document.createElement('img');
+        img.src = images[idx].src;
+        img.alt = images[idx].title;
+        img.className = "w-full h-full object-cover";
+        div.appendChild(img);
+        carousel.appendChild(div);
+    }
+}
+document.getElementById('prevBtn').onclick = function() {
+    current = (current - 1 + images.length) % images.length;
+    renderCarousel();
+};
+document.getElementById('nextBtn').onclick = function() {
+    current = (current + 1) % images.length;
+    renderCarousel();
+};
+
+                
+// funcions modal
+function openModal(title, imageSrc) {
+    const modal = document.getElementById('modal');
+    const beforeImg = document.getElementById('beforeImg');
+    const afterImg = document.getElementById('afterImg');
+    beforeImg.src =  `assets/img/${imageSrc}.png`;
+    afterImg.src = `assets/img/${imageSrc}_1.png`;
+    modal.classList.remove('hidden');
+    modal.classList.add('flex');
+    const modalTitle = document.getElementById('modalTitle');
+    modalTitle.textContent = title;
+    modal.style.zIndex = 50;
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.classList.remove('flex');
+    modal.classList.add('hidden');
+    modal.style.zIndex = 0;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            primaryBg: '#2f6da1',
+            secondaryBg: '#1A3B5C',
+            primaryText: '#ffc800',
+            secondaryText: '#1a1400',
+          }
+        }
+      }
+    }
